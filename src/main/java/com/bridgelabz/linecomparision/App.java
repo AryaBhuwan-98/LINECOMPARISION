acf2bbc41c1d438a4d9ac403e612b05b7815857e
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-01-03 22:26:47 -0800

    Add:Welcome Message for Line Comparision

---
+++ src/main/java/com/bridgelabz/linecomparision/App.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.linecomparision;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Line Comparision" );
+    }
+}
